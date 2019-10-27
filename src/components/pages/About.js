f9bd2dc41bcf6b4f0869d61056cf936d7f86e37a
marthacarolina <marthacv@gmail.com>
2019-10-26 19:16:55 -0700

    ToDoList app -React

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>
+                About 
+            </h1>
+            <p>
+                This is the Todo App v1.0.0. is part of a React crash course
+            </p>
+        </React.Fragment>
+    )
+}
+
+
